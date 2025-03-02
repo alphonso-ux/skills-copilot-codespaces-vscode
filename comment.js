80c4f9b91599e0d5a2f2157fedf5c520e4075ad6
alphonso-ux <simonjacker@proton.me>
2025-03-02 11:34:26 +0000

    copilot third commit

---
+++ comment.js
@@ -0,0 +1,6 @@
+//create a web server
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var qs = require('querystring');
+var template = require('./lib/template.js');            
